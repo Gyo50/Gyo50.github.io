f05be4acb5b0a699c6801fd8fc1dbf704141893e
gyo <oey157@nate.com>
2024-11-29 20:19:10 +0900

    s

---
+++ script/main.js
@@ -0,0 +1,5 @@
+const toggle = document.querySelector('.menu-toggle');
+const manuList = document.querySelector('.manuList');
+toggle.addEventListener(onclick,function(){
+    document.querySelector('.manuList').style.display = "block";
+})
