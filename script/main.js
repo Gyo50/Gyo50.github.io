5500fd5afca63a042f10f7cb7638a01362a6fa61
오영교 <oey157@nate.com>
2024-11-30 03:40:25 +0900

    swiper

--- script/main.js
+++ script/main.js
@@ -1,3 +1,17 @@
+/*--------------------------swiper-----------------------*/
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+});
+
+
 const toggle = document.querySelector('.menu-toggle');
 const manuList = document.querySelector('.manuList');
 toggle.addEventListener(onclick,function(){
