5ee6e489afd2c6265e745d84932ab7d4002486ab
오영교 <oey157@nate.com>
2024-12-02 05:28:09 +0900

    s

--- script/main.js
+++ script/main.js
@@ -12,8 +12,31 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 
-const toggle = document.querySelector('.menu-toggle');
-const manuList = document.querySelector('.manuList');
-toggle.addEventListener(onclick,function(){
-    document.querySelector('.manuList').style.display = "block";
-})
+function toggle() {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const manuList = document.querySelector(".manuList");
+  const logoImg = document.querySelector(".logoimg");
+  const closeElement = document.querySelector(".close");
+
+  menuToggle.style.display = "none";
+  manuList.style.display = "flex";
+  logoImg.style.backgroundImage = 'url(/mainimg/logo2.png)';
+  closeElement.style.display = "block";
+  closeElement.style.cursor = "pointer";
+
+  closeElement.addEventListener("click", () => {
+    menuToggle.style.display = "block";
+    manuList.style.display = "none";
+    logoImg.style.backgroundImage = 'url(/mainimg/logo.png)';
+    closeElement.style.display = "none";
+  });
+
+  
+}
+// width: 13px;
+// height: 13px;
+// border-top: 2px solid #fff;
+// border-right: 2px solid #fff;
+// display: inline-block;
+// transform: rotate(135deg);
+// margin: 10px;
